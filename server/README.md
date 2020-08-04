7d9121f6d16e479fd2a74f4554e2edac30036696
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2020-08-04 12:36:32 -0300

    Adicionando conexao com banco de dados
    utilizando hex e sqlite3

---
+++ server/README.md
@@ -0,0 +1,11 @@
+# Funcionalidade
+
+## Conexões
+
+- Rota para listar total de conexões;
+- Rota Para criar uma nova conexão;
+
+## Aula
+- Rota para criar uma aula;
+- Rota para listar Aulas;
+    -Filtrar por matéria,dia da semana e horários;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+//GET: buscar ou listar uma informação
+//POST: criar alguma nova informação
+//PUT: atualizar uma informação
+//DELETE: Deletar uma informação existente
 
+//Corpo (Request Body): Dados para criação ou alteração de um registro
+//Route params :identificar qual recurso quer atualizar ou deletar
+//Query Params: Paginação,filtros,ordenação
 const app =express();
-app.post('/users', (request, response) => {
-   const users = [
-       {name: 'matheus',age: 18},
-       {name: 'matheus',age :19},
-   ];
-return response.json(users);
+app.use(express.json());
+app.get('/', (request, response) => {
+
+return response.json({message: "Hello World"});
    
 });
 app.listen(3333);
