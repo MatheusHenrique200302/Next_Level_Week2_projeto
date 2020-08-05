6701d31f3576052c1bbfa193adb28550a3cc23d7
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2020-08-05 16:23:14 -0300

    alterado algus css e criado conexao com a api

--- Web/src/pages/Landing/index.tsx
+++ Web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import LogoImg from '../../assets/images/logo.svg';
 import LandingImg from '../../assets/images/landing.svg';
 import StudyIcon from '../../assets/images/icons/study.svg';
@@ -6,9 +6,21 @@ import GiveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import {Link} from 'react-router-dom';
 import './style.css'
-import { format } from 'path';
+
+
+import api from '../../services/api';
 
 function Landing(){
+    const [totalConnections,setTotalConnectios]= useState(0);
+
+useEffect(()=>{
+    api.get('connections').then(response =>{
+        const {total} = response.data;
+
+        setTotalConnectios(total);
+    })
+},[]);
+
   return(<div id="page-landing">
       <div id="page-landing-content" className="container">
         <div className="logo-container">
@@ -27,7 +39,7 @@ function Landing(){
             </Link>
         </div>
         <span className="total-connections">
-            Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+Total de  {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
         </span>
       </div>
    </div>
